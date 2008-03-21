8d22b778fbe84f7507d9ecb6ae778ebe28b7e2ad
Andrew Dupont <andrew@andrew-duponts-macbook-pro-15.local>
2008-03-21 01:14:38 -0500

    Added mouse:enter/mouse:leave custom event script.

---
+++ custom-events/mouse_enter_leave.js
@@ -0,0 +1,34 @@
+/*
+ *  Fires "mouse:enter" and "mouse:leave" events as a substitute for the
+ *  "mouseenter" and "mouseleave" events. Simulates, in effect, the behavior
+ *  of the CSS ":hover" pseudoclass.
+ */
+
+
+(function() {
+  function respondToMouseOver(event) {
+    var target = event.element();
+    if (e.relatedTarget && !e.relatedTarget.descendantOf(target))
+      target.fire("mouse:enter");
+  }
+  
+  function respondToMouseOut(event) {
+    var target = event.element();
+    if (e.relatedTarget && !e.relatedTarget.descendantOf(target))
+      target.fire("mouse:leave");
+  }
+    
+  
+  if (Prototype.Browser.IE) {
+    document.observe("mouseenter", function(event) {
+      event.element().fire("mouse:enter");
+    });
+    document.observe("mouseleave", function(event) {
+      event.element().fire("mouse:leave");
+    });
+  } else {
+    document.observe("mouseover", respondToMouseOver);
+    document.observe("mouseout",  respondToMouseOut);
+  }
+  
+})();
